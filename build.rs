ad58a5de3e296b0b9084ad41ff2f41062c0efe07
m.aay <no-reply@mally.qzz.io>
2026-05-23 18:34:28 +0700

    feat add command

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-const COMMANDS: &[&str] = &["ping", "get_installed_apps", "get_user_installed_apps"];
+const COMMANDS: &[&str] = &["ping", "get_installed_apps", "get_user_installed_apps", "get_app_tv_banner"];
 
 fn main() {
   tauri_plugin::Builder::new(COMMANDS)
